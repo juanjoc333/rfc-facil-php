97309854f59b848a8aafe83d1b85fcc0c49d859a
Juan José Cortés Guzmán <juanjoc333@gmail.com>
2016-08-09 21:33:53 -0500

    NaturalPerson class created

---
+++ src/RFC/NaturalPerson.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RFC;
+
+
+class NaturalPerson
+{
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $firstLastName;
+    /**
+     * @var string
+     */
+    public $secondLastName;
+    /**
+     * @var int
+     */
+    public $day;
+    /**
+     * @var int
+     */
+    public $month;
+    /**
+     * @var int
+     */
+    public $year;
+
+    /**
+     * NaturalPerson constructor.
+     * @param string $name
+     * @param string $firstLastName
+     * @param string $secondLastName
+     * @param int $day
+     * @param int $month
+     * @param int $year
+     */
+    public function __construct($name, $firstLastName, $secondLastName, $day, $month, $year)
+    {
+        $this->name = $name;
+        $this->firstLastName = $firstLastName;
+        $this->secondLastName = $secondLastName;
+        $this->day = $day;
+        $this->month = $month;
+        $this->year = $year;
+    }
+
+    public function getFullNameForHomoclave(){
+        return sprintf("%s %s %s", $this->firstLastName, $this->secondLastName, $this->name);
+    }
+}
